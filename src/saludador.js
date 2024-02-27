e4e88022a6706e187c1911718dd8ed1955b4c7f0
Xplay518 <gustavo.valencia@ucb.edu.bo.com>
2024-02-27 00:21:07 -0400

    3er commit, saluda segun el genero

--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,9 @@
-function saludar(nombre) {
-  return "¡Hola " + nombre + "!";
+function saludar(nombre, genero) {
+  if (genero === "femenino") {
+      return "¡Hola Sra " + nombre + "!";
+  } else {
+      return "¡Hola Sr " + nombre + "!";
+  }
 }
 
 export default saludar;
