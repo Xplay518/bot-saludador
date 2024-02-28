b1451e4550b95f1e3c5f824760cbd605ca5fb06b
Xplay518 <gustavo.valencia@ucb.edu.bo.com>
2024-02-27 22:21:21 -0400

    5to commit, se saluda con buenos dias, buenas tardes y buenas noches segun la hora

--- src/saludador.js
+++ src/saludador.js
@@ -1,12 +1,24 @@
 function saludar(nombre, genero, edad) {
+  const fecha = new Date();
+  const hora = fecha.getHours();
+  let saludo;
+
+  if (hora >= 6 && hora < 12) {
+      saludo = "Buenos días";
+  } else if (hora >= 12 && hora < 18) {
+      saludo = "Buenas tardes";
+  } else {
+      saludo = "Buenas noches";
+  }
+
   if (edad > 30) {
       if (genero === "femenino") {
-          return "¡Hola Sra " + nombre + "!";
+          return saludo + ", Sra " + nombre + "!";
       } else {
-          return "¡Hola Sr " + nombre + "!";
+          return saludo + ", Sr " + nombre + "!";
       }
   } else {
-      return "¡Hola " + nombre + "!";
+      return saludo + ", " + nombre + "!";
   }
 }
 
--- src/saludador.test.js
+++ src/saludador.test.js
@@ -2,8 +2,8 @@ import saludar from "./src/saludador.js";
 
 describe("Saludar", () => {
     it("debería saludar correctamente", () => {
-        expect(saludar("Juan", "masculino", 25)).toEqual("¡Hola Juan!");
-        expect(saludar("María", "femenino", 35)).toEqual("¡Hola Sra María!");
-        expect(saludar("Pedro", "masculino", 40)).toEqual("¡Hola Sr Pedro!");
+        expect(saludar("Juan", "masculino", 25)).toEqual("Buenos días, Juan!");
+        expect(saludar("María", "femenino", 35)).toEqual("Buenos días, Sra María!");
+        expect(saludar("Pedro", "masculino", 40)).toEqual("Buenos días, Sr Pedro!");
     });
 });
